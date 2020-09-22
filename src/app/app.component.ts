df509eeee40070ef2ea347bb131c8a0ad10dcd7a
jomsiess <jomsiess@gmail.com>
2020-09-22 15:34:11 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Satellite } from './satellite';
 
 export class AppComponent {
   sourceList: Satellite[];
+  displayList: Satellite[]; 
   constructor() {
     this.sourceList = [];
+    this.displayList = []; 
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -26,6 +28,19 @@ export class AppComponent {
     }.bind(this));
  
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
 // TODO: loop over satellites
 // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
